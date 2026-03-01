fb0aebc81742c4b55713ccf8341e3b87b8f51e42
raburton <richardaburton@gmail.com>
2026-03-01 21:17:40 +0000

    more bootsrap

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -11,13 +11,27 @@ window.hideModal = (id) => {
     if (el._bsModal) el._bsModal.hide();
 };
 
-window.showModalElement = (el) => {
+window.showModalElement = (el, options) => {
     if (!el) return;
-    if (!el._bsModal) el._bsModal = new bootstrap.Modal(el);
+    // If options provided, pass them to the Modal constructor
+    if (!el._bsModal) el._bsModal = options ? new bootstrap.Modal(el, options) : new bootstrap.Modal(el);
     el._bsModal.show();
 };
 
 window.hideModalElement = (el) => {
     if (!el) return;
     if (el._bsModal) el._bsModal.hide();
+};
+
+window.initTooltipById = (id) => {
+    const el = document.getElementById(id);
+    if (!el) return;
+    if (!el._bsTooltip) el._bsTooltip = new bootstrap.Tooltip(el);
+};
+
+window.initTooltips = () => {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+        if (!tooltipTriggerEl._bsTooltip) new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 };
